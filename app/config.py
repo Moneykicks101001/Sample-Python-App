46b7ccf2487ecb24a504b97f27153191882a2a65
Moneykicks <nhathoang101001@gmail.com>
2026-06-01 14:29:32 +0700

    re-structure repo

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os 
-class Config: 
-    APP_NAME = os.getenv("APP_NAME", "azure-demo-app") 
-    ENVIRONMENT = os.getenv("ENVIRONMENT", "dev") 
-    
-    # Future Azure integrations 
-    KEYVAULT_NAME = os.getenv("KEYVAULT_NAME", "") 
-    STORAGE_ACCOUNT = os.getenv("STORAGE_ACCOUNT", "") 
-    COSMOSDB_ENDPOINT = os.getenv("COSMOSDB_ENDPOINT", "")
